8b7a3ce91c5d4f6347cdd7187a1e14bd7281ed7d
Aziz13131313 <aza.omarov90@gmail.com>
2025-07-07 14:37:48 +0500

    convert.py

--- convert.py
+++ convert.py
@@ -1,17 +1,20 @@
 from moviepy.editor import VideoFileClip
+from pydub import AudioSegment
 import os
 
-def video_to_audio(video_path):
-    audio_path = "temp_audio.wav"
+def convert_video_to_audio(video_path):
+    audio_path = video_path.replace(".mp4", ".mp3")
+    
     try:
         video = VideoFileClip(video_path)
-        audio = video.audio
-        audio.write_audiofile(audio_path, logger=None)
-        audio.close()
-        video.close()
+        video.audio.write_audiofile(audio_path, codec='mp3')
+
+        # Дополнительная обработка через pydub (если нужно)
+        sound = AudioSegment.from_file(audio_path)
+        sound.export(audio_path, format="mp3")
+
     except Exception as e:
-        print(f"Ошибка при конвертации видео в аудио: {e}")
-        if os.path.exists(audio_path):
-            os.remove(audio_path)
+        print(f"Ошибка при конвертации видео: {e}")
         return None
+    
     return audio_path
