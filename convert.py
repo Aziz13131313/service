12e20e021f287edd487f3688535c5a62eb31d377
Aziz13131313 <aza.omarov90@gmail.com>
2025-07-08 18:55:08 +0500

    convert.py

--- convert.py
+++ convert.py
@@ -1,9 +1,9 @@
-from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.editor import VideoFileClip
 
-def convert_video_to_audio(video_path, output_format="wav"):
+def convert_video_to_audio(video_path, output_format="mp3"):
     audio_path = "temp_audio." + output_format
     video = VideoFileClip(video_path)
     audio = video.audio
-    audio.write_audiofile(audio_path)  # ДОЛЖНО идти до return!
+    audio.write_audiofile(audio_path, codec='libmp3lame')
     video.close()
     return audio_path
